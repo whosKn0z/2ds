a89db508b525fc2d63624145ce95685864efb792
q6s <89829589+q6s@users.noreply.github.com>
2022-02-22 14:14:30 +0400

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -62,7 +62,7 @@ function startMainTyping() {
 }
 
 function switchScreen() {
-  document.title = 'amplitudes.me | home';
+  document.title = 'm1ko.xyz | home';
 
   $('.intro').fadeOut(1000, function () {
     $('.bg-image').fadeIn(1000);
@@ -71,7 +71,7 @@ function switchScreen() {
     });
   });
 
-  ['background', 'rain'].forEach(function (audioName) {
+  ['rain'].forEach(function (audioName) {
     let fullPath = `assets/audio/${audioName}.mp3`;
 
     let audioElement = document.createElement('audio');
